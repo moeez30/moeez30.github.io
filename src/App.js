dfd9389664b528705bb8e07093215abbf8d67594
moeez30 <a.moeezkhan01@gmail.com>
2024-01-18 17:23:06 +0500

    App.js updated

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ import CampaignList from './pages/CampaignList';
 import AdGroupList from './pages/AdGroupList';
 import KeywordList from './pages/KeywordList';
 import Login from './pages/Login';
+import CampaignStrat from './pages/CustomStratPage';
+import SmartBiddingCampaign from './pages/SmartBiddingCampaignPage';
+import SmartBiddingAdGroup from './pages/SmartBiddingAdGroupPage';
+import SmartBiddingKeyword from './pages/SmartBiddingKeywordPage';
+import SIFeaturePage from './pages/SIFeaturePage';
+
 // import UpdateKeyword from './pages/UpdateKeyword';
+import FeaturePage from './pages/FeaturePage';
 import Header from './components/Header';
-import DataContextProvider from './context/DataContextProvider';
+// import DataContextProvider from './context/DataContextProvider';
 
 function App() {
   return (
@@ -19,7 +26,13 @@ function App() {
           <Routes> 
             {/* Regular route */}
             <Route exact path="/" element={<Login />} />
+            <Route path="/FeaturePage" element={<FeaturePage />} />
+            <Route path="/SIFeaturePage" element={<SIFeaturePage />} />
+            <Route path="/CampaignStrat" element={<CampaignStrat />} />
             <Route path="/CampaignList" element={<CampaignList />} />
+            <Route path="/SmartBiddingCampaign" element={<SmartBiddingCampaign />} />
+            <Route path="/SmartBiddingAdGroup/:id?" element={<SmartBiddingAdGroup />} />
+            <Route path="/SmartBiddingKeyword/:id?" element={<SmartBiddingKeyword />} />
             <Route path="/adGroupList/:id?" element={<AdGroupList />} />
             <Route path="/KeywordList/:id?" element={<KeywordList />} />
             {/* <Route path="/UpdateKeyword/:id?" element={UpdateKeyword} /> */}
